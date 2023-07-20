0a20e5d60b34312caf88b49180339490a5c3f119
Jerald Torres <115351172+AkosiJerald@users.noreply.github.com>
2023-07-20 14:17:26 +0800

    Add files via upload

---
+++ prob1.py
@@ -0,0 +1,16 @@
+gram =float(input("Enter gram: "))
+
+kilo = gram / 1000
+miligram = gram * 1000
+microgram = gram * 1000000
+pound = gram * 453.6
+ounce = gram / 28.35
+liter = gram / 1000
+
+print(gram, "Gram to kilogram =", kilo, "\n",
+      gram, "Gram to miligram =", miligram, "\n",
+      gram, "Gram to microgram =", microgram, "\n",
+      gram, "Gram to pound =", pound, "\n",
+      gram, "Gram to ounce =", ounce, "\n",
+      gram, "Gram to liter =", liter)
+
